742f5d97670271086c828d2f6fd7c137b57db67b
Eloi Torrents <eloitor@duck.com>
2025-05-25 08:19:42 +0200

    Add euclidean domain structure to naturals

--- rings/src/rings/natural/mod.rs
+++ rings/src/rings/natural/mod.rs
@@ -1,5 +1,5 @@
 use crate::structure::*;
-use algebraeon_nzq::*;
+use algebraeon_nzq::{traits::DivMod, *};
 use algebraeon_sets::structure::*;
 
 pub mod factorization;
@@ -23,5 +23,44 @@ impl SemiRingSignature for NaturalCanonicalStructure {
 impl CharacteristicSignature for NaturalCanonicalStructure {
     fn characteristic(&self) -> Natural {
         Natural::ZERO
+    }
+}
+
+impl UnitsSignature for NaturalCanonicalStructure {
+    fn inv(&self, a: &Self::Set) -> Result<Self::Set, RingDivisionError> {
+        self.div(&self.one(), a)
+    }
+}
+
+impl IntegralDomainSignature for NaturalCanonicalStructure {
+    fn div(&self, a: &Self::Set, b: &Self::Set) -> Result<Self::Set, RingDivisionError> {
+        match self.quorem(a, b) {
+            Some((q, r)) => {
+                if r == self.zero() {
+                    Ok(q)
+                } else {
+                    Err(RingDivisionError::NotDivisible)
+                }
+            }
+            None => Err(RingDivisionError::DivideByZero),
+        }
+    }
+}
+
+impl EuclideanDivisionSignature for NaturalCanonicalStructure {
+    fn norm(&self, elem: &Self::Set) -> Option<Natural> {
+        if elem == &Natural::ZERO {
+            None
+        } else {
+            Some(elem.clone())
+        }
+    }
+
+    fn quorem(&self, a: &Self::Set, b: &Self::Set) -> Option<(Self::Set, Self::Set)> {
+        if b == &Natural::ZERO {
+            None
+        } else {
+            Some(a.div_mod(b))
+        }
     }
 }
